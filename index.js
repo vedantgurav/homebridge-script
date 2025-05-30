f2a6e05756ef6f32002c7f4921686b37e34224bf
Vedant Gurav <hello@vedantgurav.com>
2025-05-30 08:58:48 +0100

    Adapt to lock

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-var Service;
-var Characteristic;
+import { exec } from "node:child_process";
+import { sync } from "node:file-exists";
 
-var sys = require('sys');
-    exec = require('child_process').exec;
-    assign = require('object-assign');
-    fileExists = require('file-exists');
+let Service;
+let Characteristic;
 
-module.exports = function(homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-script', 'Script', scriptAccessory);
-}
+export default (homebridge) => {
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	homebridge.registerAccessory("homebridge-script", "Script", scriptAccessory);
+};
 
 function puts(error, stdout, stderr) {
-   console.log(stdout)
+	console.log(stdout);
+	if (stderr) {
+		console.error("stderr: ", stderr);
+	}
+	if (error) {
+		console.error("error: ", error);
+	}
 }
 
 function scriptAccessory(log, config) {
-  this.log = log;
-  this.service = 'Switch';
+	this.log = log;
+	this.service = "LockMechanism";
 
-  this.name = config['name'];
-  this.onCommand = config['on'];
-  this.offCommand = config['off'];
-  this.stateCommand = config['state'];
-  this.onValue = config['on_value'];
-  this.fileState = config['fileState'] || false;
-  this.onValue = this.onValue.trim().toLowerCase();
-  //this.exactMatch = config['exact_match'] || true;
+	this.name = config.name;
+	this.onCommand = config.on;
+	this.offCommand = config.off;
+	this.stateCommand = config.state;
+	this.onValue = config.on_value;
+	this.fileState = config.fileState || false;
+	this.onValue = this.onValue.trim().toLowerCase();
+	//this.exactMatch = config['exact_match'] || true;
 }
 
 /* 
@@ -40,51 +44,108 @@ function scriptAccessory(log, config) {
   }
 }
 */
-scriptAccessory.prototype.setState = function(powerOn, callback) {
-  var accessory = this;
-  var state = powerOn ? 'on' : 'off';
-  var prop = state + 'Command';
-  var command = accessory[prop];
+scriptAccessory.prototype.setState = function (lockState, callback) {
+	const targetState =
+		lockState === Characteristic.LockTargetState.UNSECURED
+			? "UNSECURED"
+			: "SECURED";
+	const command =
+		lockState === Characteristic.LockTargetState.UNSECURED
+			? this.onCommand
+			: this.offCommand;
 
-    exec(command, puts);
-    accessory.log('Set ' + accessory.name + ' to ' + state);
-    callback(null);
-}
+	if (!command) {
+		this.log.warn(`No command defined for ${targetState} state.`);
+		const currentLockStateVal = this.serviceInstance.getCharacteristic(
+			Characteristic.LockCurrentState,
+		).value;
+		if (lockState === currentLockStateVal) {
+			this.log(`State is already ${targetState}.`);
+			callback(null);
+			return;
+		}
+		callback(new Error(`No command defined for ${targetState}`));
+		return;
+	}
 
-scriptAccessory.prototype.getState = function(callback) {
-  var accessory = this;
-  var command = accessory['stateCommand'];
-  var stdout = "none";  
-  
-  if (this.fileState) {
-    var flagFile = fileExists.sync(this.fileState);
-    accessory.log('State of ' + accessory.name + ' is: ' + flagFile);
-    callback(null, flagFile);
-  }
-  else {
-    exec(command, function (error, stdout, stderr) {
-      var cleanOut=stdout.trim().toLowerCase();
-      accessory.log('State of ' + accessory.name + ' is: ' + cleanOut);
-      callback(null, cleanOut == accessory.onValue);
-    });
-  }
-}
+	exec(command, (error, stdout, stderr) => {
+		puts(error, stdout, stderr);
+		if (error) {
+			this.log.error(
+				`Failed to set ${this.name} to ${targetState}: ${error.message}`,
+			);
+			callback(error);
+		} else {
+			this.log(`Set ${this.name} to ${targetState}`);
+			const newCurrentState =
+				lockState === Characteristic.LockTargetState.UNSECURED
+					? Characteristic.LockCurrentState.UNSECURED
+					: Characteristic.LockCurrentState.SECURED;
+			this.serviceInstance.updateCharacteristic(
+				Characteristic.LockCurrentState,
+				newCurrentState,
+			);
+			callback(null);
+		}
+	});
+};
 
-scriptAccessory.prototype.getServices = function() {
-  var informationService = new Service.AccessoryInformation();
-  var switchService = new Service.Switch(this.name);
+scriptAccessory.prototype.getState = function (callback) {
+	const command = this.stateCommand;
+	const stdout = "none";
 
-  informationService
-  .setCharacteristic(Characteristic.Manufacturer, 'script Manufacturer')
-  .setCharacteristic(Characteristic.Model, 'script Model')
-  .setCharacteristic(Characteristic.SerialNumber, 'script Serial Number');
+	if (this.fileState) {
+		const flagFile = sync(this.fileState);
+		const currentState = flagFile
+			? Characteristic.LockCurrentState.UNSECURED
+			: Characteristic.LockCurrentState.SECURED;
+		this.log(
+			`Current state of ${this.name} (file) is: ${currentState === Characteristic.LockCurrentState.UNSECURED ? "UNSECURED" : "SECURED"}`,
+		);
+		callback(null, currentState);
+	} else {
+		exec(command, (error, stdout, stderr) => {
+			if (error) {
+				this.log.error(`Error getting state: ${error.message}`);
+				callback(error);
+				return;
+			}
+			const cleanOut = stdout.trim().toLowerCase();
+			const currentState =
+				cleanOut === this.onValue
+					? Characteristic.LockCurrentState.UNSECURED
+					: Characteristic.LockCurrentState.SECURED;
+			this.log(
+				`State of ${this.name} is: ${currentState === Characteristic.LockCurrentState.UNSECURED ? "UNSECURED" : "SECURED"} (raw: "${cleanOut}")`,
+			);
+			callback(null, currentState);
+		});
+	}
+};
 
-  var characteristic = switchService.getCharacteristic(Characteristic.On)
-  .on('set', this.setState.bind(this));
+scriptAccessory.prototype.getServices = function () {
+	const informationService = new Service.AccessoryInformation();
+	this.serviceInstance = new Service.LockMechanism(this.name);
 
-  if (this.stateCommand || this.fileState) {
-    characteristic.on('get', this.getState.bind(this))
-  };
+	informationService
+		.setCharacteristic(Characteristic.Manufacturer, "script Manufacturer")
+		.setCharacteristic(Characteristic.Model, "script Model")
+		.setCharacteristic(Characteristic.SerialNumber, "script Serial Number");
 
-  return [switchService];
-}
+	this.serviceInstance
+		.getCharacteristic(Characteristic.LockTargetState)
+		.on("set", this.setState.bind(this));
+
+	if (this.stateCommand || this.fileState) {
+		this.serviceInstance
+			.getCharacteristic(Characteristic.LockCurrentState)
+			.on("get", this.getState.bind(this));
+	} else {
+		this.serviceInstance.setCharacteristic(
+			Characteristic.LockCurrentState,
+			Characteristic.LockCurrentState.SECURED,
+		);
+	}
+
+	return [informationService, this.serviceInstance];
+};
